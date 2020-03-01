fb4404075e539e520deb9a294666de54a8fafae8
KK578 <KevinKwan95@googlemail.com>
2020-03-01 01:00:26 +0000

    BASHI-2: Create The Little Mandarin header bar and navigation  - Add basic text content | Move Public Site to Blazor

---
+++ Mandarin/gulpfile.js
@@ -0,0 +1,21 @@
+﻿const gulp = require("gulp");
+
+function css() {
+    const postcss = require("gulp-postcss");
+    const cleanCss = require("gulp-clean-css");
+    const sourceMaps = require("gulp-sourcemaps");
+
+    return gulp.src("./Styles/*.css")
+        .pipe(postcss([
+            require("precss"),
+            require("tailwindcss"),
+            require("autoprefixer")
+        ]))
+        .pipe(sourceMaps.init())
+        .pipe(cleanCss())
+        .pipe(sourceMaps.write("./"))
+        .pipe(gulp.dest("./wwwroot/css/"));
+}
+
+gulp.task(css);
+gulp.task("default", css);
