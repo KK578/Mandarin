acf5cc084677b03806757420c2aca2c676b68c7b
KK578 <KevinKwan95@googlemail.com>
2020-03-01 11:56:05 +0000

    BASHI-3: Move Mandarin.csproj to src/ directory | Move Home Page to Blazor

---
+++ src/Mandarin/MandarinStartup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Mandarin
+{
+    /// <summary>
+    /// ASP.NET Core services configuration for Mandarin public-facing application.
+    /// </summary>
+    public class MandarinStartup
+    {
+        /// <summary>
+        /// Configures the provided service collection with all the dependencies required for Mandarin.
+        /// <br />
+        /// Currently this includes:
+        /// <list type="bullet">
+        /// <listheader>
+        /// <term>Razor</term>
+        /// </listheader>
+        /// </list>
+        ///
+        /// <remarks>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// For more information on how to configure your application, visit <a href="https://go.microsoft.com/fwlink/?LinkID=398940">https://go.microsoft.com/fwlink/?LinkID=398940</a>.
+        /// </remarks>
+        /// </summary>
+        /// <param name="services">Service collection to be modified.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+        }
+
+        /// <summary>
+        /// Configures the HTTP Pipeline for Mandarin.
+        ///
+        /// <remarks>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </remarks>
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app)
+        {
+            var env = app.ApplicationServices.GetService<IWebHostEnvironment>();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
