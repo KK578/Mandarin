8bf9ef99d2ad1bef0a626894ef7a7095a0a18b04
KK578 <KevinKwan95@googlemail.com>
2020-03-02 20:26:12 +0000

    BASHI-3: Fix new SonarCloud issues | Move Home Page to Blazor

--- src/Mandarin.ViewModels/Index/Carousel/CarouselImageViewModel.cs
+++ src/Mandarin.ViewModels/Index/Carousel/CarouselImageViewModel.cs
@@ -6,15 +6,19 @@ namespace Mandarin.ViewModels.Index.Carousel
     {
         public CarouselImageViewModel(string sourceUrl, string description)
         {
-            SourceUrl = Uri.IsWellFormedUriString(sourceUrl, UriKind.Absolute)
-                ? new Uri(sourceUrl)
-                : Uri.IsWellFormedUriString(sourceUrl, UriKind.Relative)
-                    ? new Uri(sourceUrl, UriKind.Relative)
-                    : throw new ArgumentException($"Invalid sourceUrl {sourceUrl}", nameof(sourceUrl));
+            SourceUrl = ParseUri(sourceUrl) ?? throw new ArgumentException($"Invalid sourceUrl {sourceUrl}", nameof(sourceUrl));
             Description = description;
         }
 
         public Uri SourceUrl { get; }
         public string Description { get; }
+
+        private static Uri ParseUri(string sourceUrl)
+        {
+            if (Uri.IsWellFormedUriString(sourceUrl, UriKind.Absolute))
+                return new Uri(sourceUrl);
+
+            return Uri.IsWellFormedUriString(sourceUrl, UriKind.Relative) ? new Uri(sourceUrl, UriKind.Relative) : null;
+        }
     }
 }
--- src/Mandarin.ViewModels/Index/Carousel/CarouselViewModel.cs
+++ src/Mandarin.ViewModels/Index/Carousel/CarouselViewModel.cs
@@ -10,10 +10,10 @@ namespace Mandarin.ViewModels.Index.Carousel
         {
             Images = new List<ICarouselImageViewModel>
             {
-                new CarouselImageViewModel("https://thelittlemandarin.co.uk/static/images/about/TheLittleMandarin-1.jpg", "The Little Mandarin - Shop Front"),
-                new CarouselImageViewModel("https://thelittlemandarin.co.uk/static/images/about/TheLittleMandarin-2.jpg", "The Little Mandarin - Display"),
-                new CarouselImageViewModel("https://thelittlemandarin.co.uk/static/images/about/TheLittleMandarin-3.jpg", "The Little Mandarin - Interior"),
-                new CarouselImageViewModel("https://thelittlemandarin.co.uk/static/images/about/TheLittleMandarin-4.jpg", "The Mini Mandarin - Macarons"),
+                new CarouselImageViewModel("/images/about/ShopFront.jpg", "The Little Mandarin - Shop Front"),
+                new CarouselImageViewModel("/images/about/ShopWallDisplay.jpg", "The Little Mandarin - Display"),
+                new CarouselImageViewModel("/images/about/ShopDisplay.jpg", "The Little Mandarin - Interior"),
+                new CarouselImageViewModel("/images/about/Macarons.jpg", "The Mini Mandarin - Macarons"),
             }.AsReadOnly();
         }
     }
