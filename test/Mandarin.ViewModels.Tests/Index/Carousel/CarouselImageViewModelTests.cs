58ee9e30ea23dd0bfa39ee727f06926f22a62ddb
KK578 <KevinKwan95@googlemail.com>
2020-03-02 20:11:00 +0000

    BASHI-3: Exclude compiled Mandarin.Views assembly | Move Home Page to Blazor

--- test/Mandarin.ViewModels.Tests/Index/Carousel/CarouselImageViewModelTests.cs
+++ test/Mandarin.ViewModels.Tests/Index/Carousel/CarouselImageViewModelTests.cs
@@ -8,9 +8,24 @@ namespace Mandarin.ViewModels.Tests.Index.Carousel
     public class CarouselImageViewModelTests
     {
         [Test]
+        public void Constructor_GivenInvalidUrl_ShouldThrowArgumentException()
+        {
+            const string sourceUrl = "/oops#that#doesn't#work";
+            Assert.That(() => new CarouselImageViewModel(sourceUrl, TestData.NextString()), Throws.ArgumentException);
+        }
+
+        [Test]
         public void SourceUrl_ShouldReturnValueFromConstructor()
         {
             const string sourceUrl = "https://localhost/image.png";
+            var subject = new CarouselImageViewModel(sourceUrl, TestData.NextString());
+            Assert.That(subject.SourceUrl.ToString(), Is.EqualTo(sourceUrl));
+        }
+
+        [Test]
+        public void SourceUrl_GivenRelativeUrl_ShouldReturnValueFromConstructor()
+        {
+            const string sourceUrl = "/image.png";
             var subject = new CarouselImageViewModel(sourceUrl, TestData.NextString());
             Assert.That(subject.SourceUrl.ToString(), Is.EqualTo(sourceUrl));
         }
