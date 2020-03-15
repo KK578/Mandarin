ecc369b447442e133dbd86e9a49d9819d92be655
Kevin Kwan <KevinKwan95@googlemail.com>
2020-03-15 16:19:36 +0000

    BASHI-33: Add Octopus Build Information. (#14)

--- src/Mandarin/gulpfile.js
+++ src/Mandarin/gulpfile.js
@@ -1,5 +1,54 @@
 ﻿const gulp = require("gulp");
 
+async function buildInformation() {
+    const simpleGit = require("simple-git/promise");
+    const fancyLog = require("fancy-log");
+    const git = simpleGit();
+    const fs = require("fs");
+    const packageJson = require("./package.json");
+
+    const fromVersionSha = process.env["GitVersion_VersionSourceSha"];
+    const toVersionSha = process.env["GitVersion_Sha"];
+    const buildNumber = process.env["GITHUB_RUN_ID"];
+
+    if (!fromVersionSha || !toVersionSha || !buildNumber)
+    {
+        fancyLog.warn("Not running on GitHub CI.");
+        fancyLog.info("To run locally, please set the following environment variables:");
+        if (!fromVersionSha)
+            fancyLog.info(" - GitVersion_VersionSourceSha = [From Version SHA]");
+        if (!toVersionSha)
+            fancyLog.info(" - GitVersion_Sha = [To Version SHA]")
+        if (!buildNumber)
+            fancyLog.info(" - GITHUB_RUN_ID = [Github Action run identifier]")
+        return;
+    }
+
+    const rawCommits = await git.raw(["log", "--pretty=format:%H|%s", `${fromVersionSha}..${toVersionSha}`]);
+    const commits = rawCommits.split("\n")
+        .filter(x => x)
+        .map(x => x.split("|"))
+        .map(x => ({
+            id: x[0],
+            comment: x.splice(1).join("|")
+        }));
+    fancyLog.info(`Found ${commits.length} commits.`);
+
+    const info = {
+        buildEnvironment: "GitHub Actions",
+        commentParser: "JIRA",
+        buildNumber: buildNumber,
+        buildUrl: `${packageJson.repository.url}/actions/runs/${buildNumber}`,
+        vcsType: "Git",
+        vcsRoot: packageJson.repository.url,
+        vcsCommitNumber: toVersionSha,
+        commits: commits
+    };
+    fancyLog.info(info);
+
+    fs.writeFileSync("BuildInformation.json", JSON.stringify(info));
+}
+
 function css() {
     const debug = require("gulp-debug");
     const postcss = require("gulp-postcss");
@@ -26,5 +75,6 @@ function css() {
         }))
 }
 
+gulp.task(buildInformation);
 gulp.task(css);
 gulp.task("default", css);
