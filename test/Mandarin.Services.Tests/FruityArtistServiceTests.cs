70c6551bac1266dec7f145d7c02c0ecba5957ec3
KK578 <KevinKwan95@googlemail.com>
2020-04-12 22:46:36 +0100

    BASHI-12: Add minimal artist data test | Move Artists Page to Blazor

--- test/Mandarin.Services.Tests/FruityArtistServiceTests.cs
+++ test/Mandarin.Services.Tests/FruityArtistServiceTests.cs
@@ -15,6 +15,9 @@ namespace Mandarin.Services.Tests
     [TestFixture]
     public class FruityArtistServiceTests
     {
+        private const string MinimalArtistData = @"[{""stockist_name"": ""Artist Name"", ""description"": ""Artist's Description."", ""image_url"": ""https://localhost/images/artist1.jpg""}]";
+        private const string FullArtistData = @"[{""stockist_name"": ""Artist Name"", ""description"": ""Artist's Description."", ""image_url"": ""https://localhost/images/artist1.jpg"", ""twitter_handle"": ""ArtistTwitter"", ""instagram_handle"": ""ArtistInstagram"", ""facebook_handle"": ""ArtistFacebook"", ""tumblr_handle"": ""ArtistTumblr"", ""website_url"": ""https://localhost/artist/website"" }]";
+
         private Mock<HttpMessageHandler> messageHandler;
         private HttpClient httpClient;
 
@@ -29,9 +32,25 @@ namespace Mandarin.Services.Tests
         }
 
         [Test]
+        public async Task GetArtistDetail_GivenMinimalJsonDataFromService_ShouldDeserializeCorrectly()
+        {
+            GivenFruityClientReturns(FruityArtistServiceTests.MinimalArtistData);
+            var artistDetails = await WhenGetArtistDetail();
+            Assert.That(artistDetails, Has.Count.EqualTo(1));
+            Assert.That(artistDetails[0].Name, Is.EqualTo("Artist Name"));
+            Assert.That(artistDetails[0].Description, Is.EqualTo("Artist's Description."));
+            Assert.That(artistDetails[0].ImageUrl, Is.EqualTo(new Uri("https://localhost/images/artist1.jpg")));
+            Assert.That(artistDetails[0].TwitterUrl, Is.Null);
+            Assert.That(artistDetails[0].InstagramUrl, Is.Null);
+            Assert.That(artistDetails[0].FacebookUrl, Is.Null);
+            Assert.That(artistDetails[0].TumblrUrl, Is.Null);
+            Assert.That(artistDetails[0].WebsiteUrl, Is.Null);
+        }
+
+        [Test]
         public async Task GetArtistDetail_GivenJsonDataFromService_ShouldDeserializeCorrectly()
         {
-            GivenFruityClientReturnsWellKnownData();
+            GivenFruityClientReturns(FruityArtistServiceTests.FullArtistData);
             var artistDetails = await WhenGetArtistDetail();
             Assert.That(artistDetails, Has.Count.EqualTo(1));
             Assert.That(artistDetails[0].Name, Is.EqualTo("Artist Name"));
@@ -44,27 +63,22 @@ namespace Mandarin.Services.Tests
             Assert.That(artistDetails[0].WebsiteUrl, Is.EqualTo(new Uri("https://localhost/artist/website")));
         }
 
+        private void GivenFruityClientReturns(string data)
+        {
+            this.messageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(data)
+                });
+        }
+
         private Task<IReadOnlyList<ArtistDetailsModel>> WhenGetArtistDetail()
         {
             var subject = new FruityArtistService(this.httpClient);
             return subject.GetArtistDetails();
-        }
-
-        private void GivenFruityClientReturnsWellKnownData()
-        {
-            var artistData = @"[{""stockist_name"": ""Artist Name"", ""description"": ""Artist's Description."",
-""image_url"": ""https://localhost/images/artist1.jpg"", ""twitter_handle"": ""ArtistTwitter"", ""instagram_handle"": ""ArtistInstagram"",
-""facebook_handle"": ""ArtistFacebook"", ""tumblr_handle"": ""ArtistTumblr"", ""website_url"": ""https://localhost/artist/website"" }]";
-
-            this.messageHandler.Protected()
-                .Setup<Task<HttpResponseMessage>>("SendAsync",
-                                                  ItExpr.IsAny<HttpRequestMessage>(),
-                                                  ItExpr.IsAny<CancellationToken>())
-                .ReturnsAsync(new HttpResponseMessage
-                {
-                    StatusCode = HttpStatusCode.OK,
-                    Content = new StringContent(artistData)
-                });
         }
     }
 }
