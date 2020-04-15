62e6db710bdcadf90c8ae63e7f4dc39effa340ca
Kevin Kwan <KevinKwan95@googlemail.com>
2020-04-15 20:10:42 +0100

    BASHI-39: Complete Configuration for Contact Page. (#35)

--- test/Mandarin.Services.Tests/Email/SendGridEmailServiceTests.cs
+++ test/Mandarin.Services.Tests/Email/SendGridEmailServiceTests.cs
@@ -7,10 +7,12 @@ using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Bashi.Tests.Framework.Logging;
 using BlazorInputFile;
 using Mandarin.Models.Contact;
 using Mandarin.Services.Email;
 using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
 using Moq;
 using NUnit.Framework;
 using SendGrid;
@@ -21,6 +23,21 @@ namespace Mandarin.Services.Tests.Email
     [TestFixture]
     public class SendGridEmailServiceTests
     {
+        private const string ServiceEmail = "noreply@example.com";
+        private const string RealContactEmail = "contact@example.com";
+
+        private IOptions<SendGridConfiguration> configuration;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.configuration = Options.Create(new SendGridConfiguration
+            {
+                ServiceEmail = SendGridEmailServiceTests.ServiceEmail,
+                RealContactEmail = SendGridEmailServiceTests.RealContactEmail,
+            });
+        }
+
         [Test]
         public void BuildEmailAsync_GivenAnInvalidModel_ShouldThrowException()
         {
@@ -32,7 +49,7 @@ namespace Mandarin.Services.Tests.Email
                 Comment = "My extra long comment.",
             };
 
-            var subject = new SendGridEmailService(Mock.Of<ISendGridClient>(), NullLogger<SendGridEmailService>.Instance);
+            var subject = new SendGridEmailService(Mock.Of<ISendGridClient>(), this.configuration, NullLogger<SendGridEmailService>.Instance);
 
             Assert.That(() => subject.BuildEmailAsync(model), Throws.Exception.InstanceOf<ValidationException>());
         }
@@ -48,10 +65,13 @@ namespace Mandarin.Services.Tests.Email
                 Comment = "My extra long comment."
             };
 
-            var subject = new SendGridEmailService(Mock.Of<ISendGridClient>(), NullLogger<SendGridEmailService>.Instance);
+            var subject =
+                new SendGridEmailService(Mock.Of<ISendGridClient>(), this.configuration, NullLogger<SendGridEmailService>.Instance);
             var result = await subject.BuildEmailAsync(model);
 
-            Assert.That(result.From.Email, Is.EqualTo("MyValid@Email.com"));
+            Assert.That(result.From.Email, Is.EqualTo(SendGridEmailServiceTests.ServiceEmail));
+            Assert.That(result.ReplyTo.Email, Is.EqualTo("MyValid@Email.com"));
+            Assert.That(result.Personalizations[0].Tos[0].Email, Is.EqualTo(SendGridEmailServiceTests.RealContactEmail));
             Assert.That(result.Subject, Is.EqualTo("My Name - General Query"));
             Assert.That(result.PlainTextContent.Replace("\r\n", "|").Replace("\n", "|"),
                         Is.EqualTo("Reason: General Query||Comment:|My extra long comment.|"));
@@ -69,7 +89,8 @@ namespace Mandarin.Services.Tests.Email
                 Comment = "My extra long comment."
             };
 
-            var subject = new SendGridEmailService(Mock.Of<ISendGridClient>(), NullLogger<SendGridEmailService>.Instance);
+            var subject =
+                new SendGridEmailService(Mock.Of<ISendGridClient>(), this.configuration, NullLogger<SendGridEmailService>.Instance);
             var result = await subject.BuildEmailAsync(model);
 
             Assert.That(result.Subject, Is.EqualTo("My Name - Other (My Special Reason)"));
@@ -88,15 +109,46 @@ namespace Mandarin.Services.Tests.Email
                 Comment = "My extra long comment.",
                 Attachments = new List<IFileListEntry>
                 {
-                    Mock.Of<IFileListEntry>(x => x.Name == "File.zip" && x.Size == 1024 && x.Data == data && x.Type == "zip")
+                    Mock.Of<IFileListEntry>(x => x.Name == "File.zip" && x.Size == 1024 && x.Data == data &&
+                                                 x.Type == "zip")
                 }
             };
 
-            var subject = new SendGridEmailService(Mock.Of<ISendGridClient>(), NullLogger<SendGridEmailService>.Instance);
+            var subject =
+                new SendGridEmailService(Mock.Of<ISendGridClient>(), this.configuration, NullLogger<SendGridEmailService>.Instance);
             var result = await subject.BuildEmailAsync(model);
 
             Assert.That(result.Attachments.Count, Is.EqualTo(1));
             Assert.That(Convert.FromBase64String(result.Attachments[0].Content), Is.EqualTo("Attachment"));
+        }
+
+        [Test]
+        public async Task SendEmailAsync_GivenErrorResponse_BodyIsLogged_AndStatusCodeIsCopiedToResponse()
+        {
+            var email = new SendGridMessage();
+            var sendGridClient = new Mock<ISendGridClient>();
+            sendGridClient.Setup(x => x.SendEmailAsync(email, It.IsAny<CancellationToken>()))
+                          .ReturnsAsync(new Response(HttpStatusCode.Unauthorized, new StringContent("Invalid API Key"), null));
+            var logger = new TestableLogger<SendGridEmailService>();
+            var subject = new SendGridEmailService(sendGridClient.Object, this.configuration, logger);
+            var result = await subject.SendEmailAsync(email);
+
+            Assert.That(result.IsSuccess, Is.False);
+            Assert.That(result.StatusCode, Is.EqualTo(401));
+            Assert.That(logger.LogEntries.Count, Is.EqualTo(1));
+            Assert.That(logger.LogEntries[0].Message, Contains.Substring("Invalid API Key"));
+        }
+
+        [Test]
+        public void SendEmailAsync_GivenResponseBodyIsNull_NoExceptionIsThrown()
+        {
+            var email = new SendGridMessage();
+            var sendGridClient = new Mock<ISendGridClient>();
+            sendGridClient.Setup(x => x.SendEmailAsync(email, It.IsAny<CancellationToken>()))
+                          .ReturnsAsync(new Response(HttpStatusCode.Accepted, null, null));
+            var subject = new SendGridEmailService(sendGridClient.Object, this.configuration, NullLogger<SendGridEmailService>.Instance);
+
+            Assert.DoesNotThrowAsync(() => subject.SendEmailAsync(email));
         }
 
         [Test]
@@ -106,7 +158,7 @@ namespace Mandarin.Services.Tests.Email
             var sendGridClient = new Mock<ISendGridClient>();
             sendGridClient.Setup(x => x.SendEmailAsync(email, It.IsAny<CancellationToken>()))
                           .ReturnsAsync(new Response(HttpStatusCode.Accepted, new StringContent(""), null));
-            var subject = new SendGridEmailService(sendGridClient.Object, NullLogger<SendGridEmailService>.Instance);
+            var subject = new SendGridEmailService(sendGridClient.Object, this.configuration, NullLogger<SendGridEmailService>.Instance);
             var result = await subject.SendEmailAsync(email);
 
             Assert.That(result.IsSuccess, Is.True);
