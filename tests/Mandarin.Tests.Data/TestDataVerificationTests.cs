184bd92a2cd435ae1e266ed1eafffb39b49e1e41
KK578 <KevinKwan95@googlemail.com>
2020-04-15 23:32:37 +0100

    BASHI-40: Exclude Tests.Data from CodeCoverage | Move compilation to single build folder

---
+++ tests/Mandarin.Tests.Data/TestDataVerificationTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace Mandarin.Tests.Data
+{
+    [TestFixture]
+    public class TestDataVerificationTests
+    {
+        [Test]
+        [TestCaseSource(nameof(TestDataVerificationTests.ListAllFileNames))]
+        public void VerifyFileExists(string filename)
+        {
+            FileAssert.Exists(filename);
+        }
+
+        public static IEnumerable<string> ListAllFileNames()
+        {
+            var assemblyTypes = typeof(TestDataVerificationTests).Assembly.GetTypes();
+            var staticTypes = assemblyTypes.Where(t => t.IsClass && t.IsSealed && t.IsAbstract);
+            var constFields = staticTypes.SelectMany(x => x.GetFields(BindingFlags.Public | BindingFlags.Static));
+            foreach (var constantField in constFields)
+            {
+                var value = constantField.GetValue(null);
+                if (value is string s)
+                {
+                    yield return s;
+                }
+            }
+        }
+    }
+}
