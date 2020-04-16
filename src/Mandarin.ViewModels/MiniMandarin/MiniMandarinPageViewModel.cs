913cb8c09ab97797509187ec197e978fd7cf84cf
Kevin Kwan <KevinKwan95@googlemail.com>
2020-04-16 08:17:00 +0100

    BASHI-45: Serve Static Resources under /static. (#40)

--- src/Mandarin.ViewModels/MiniMandarin/MiniMandarinPageViewModel.cs
+++ src/Mandarin.ViewModels/MiniMandarin/MiniMandarinPageViewModel.cs
@@ -12,12 +12,12 @@ namespace Mandarin.ViewModels.MiniMandarin
                 "The Mini Mandarin is the younger sister of The Little Mandarin and is run by the baker of yummy treats in the family. Come drop by for handmade and hand finished ‘bearcarons’ – bear shaped macarons! Choose from three delightful flavours, chocolate, strawberry and matcha! Other special flavours pop up all year round and JUMBO sized macarons too, so come back to see what's new! The Mini Mandarin also offers a range of sweets and chocolates suitable for vegetarians and vegans - there's a treat for everyone!"
             }.AsReadOnly();
 
-            this.BannerImageViewModel = new MandarinImageViewModel("/images/the-mini-mandarin/TheMiniMandarin-Banner.jpg", "The Mini Mandarin - Bearcarons");
+            this.BannerImageViewModel = new MandarinImageViewModel("/static/images/the-mini-mandarin/TheMiniMandarin-Banner.jpg", "The Mini Mandarin - Bearcarons");
             this.MacaronImageViewModels = new List<IMandarinImageViewModel>
             {
-                new MandarinImageViewModel("/images/the-mini-mandarin/TheMiniMandarin-Macaron-Chocolate.jpg", "The Mini Mandarin - Chocolate Macaron"),
-                new MandarinImageViewModel("/images/the-mini-mandarin/TheMiniMandarin-Macaron-Strawberry.jpg", "The Mini Mandarin - Strawberry Macaron"),
-                new MandarinImageViewModel("/images/the-mini-mandarin/TheMiniMandarin-Macaron-Matcha.jpg", "The Mini Mandarin - Matcha Macaron"),
+                new MandarinImageViewModel("/static/images/the-mini-mandarin/TheMiniMandarin-Macaron-Chocolate.jpg", "The Mini Mandarin - Chocolate Macaron"),
+                new MandarinImageViewModel("/static/images/the-mini-mandarin/TheMiniMandarin-Macaron-Strawberry.jpg", "The Mini Mandarin - Strawberry Macaron"),
+                new MandarinImageViewModel("/static/images/the-mini-mandarin/TheMiniMandarin-Macaron-Matcha.jpg", "The Mini Mandarin - Matcha Macaron"),
             }.AsReadOnly();
         }
 
