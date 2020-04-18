d85aeefdeb302a492e906eb87780c5550b19b493
KK578 <KevinKwan95@googlemail.com>
2020-04-18 01:18:33 +0100

    BASHI-46: Make WireMock static instanced | Fix Flaky Tests due to Fruity WireMock

---
+++ tests/Mandarin.Tests/Mocks/FruityMock.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using Mandarin.Tests.Data;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace Mandarin.Tests.Mocks
+{
+    internal static class FruityMock
+    {
+        private static WireMockServer server;
+
+        public static void EnsureStarted()
+        {
+            FruityMock.server ??= FruityMock.CreateServer();
+        }
+
+        private static WireMockServer CreateServer()
+        {
+            var wireMock = WireMockServer.Start("http://localhost:9090");
+            wireMock.Given(Request.Create().WithPath("/api/stockist"))
+                    .RespondWith(Response.Create()
+                                         .WithStatusCode(HttpStatusCode.OK)
+                                         .WithBodyFromFile(WellKnownTestData.Fruity.Stockist.TheLittleMandarin));
+
+            return wireMock;
+        }
+    }
+}
