d6413f02bb73b7ecfb4c0f400df2957a6f4d87e0
KK578 <KevinKwan95@googlemail.com>
2020-06-21 23:23:10 +0100

    BASHI-4: Add Mini Mandarin content to JSON | Move page contents to external structure

--- src/Mandarin.ViewModels/MiniMandarin/MiniMandarinPageViewModel.cs
+++ src/Mandarin.ViewModels/MiniMandarin/MiniMandarinPageViewModel.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
 using Mandarin.ViewModels.Components.Images;
-using Markdig;
 using Microsoft.AspNetCore.Components;
 
 namespace Mandarin.ViewModels.MiniMandarin
@@ -11,18 +11,15 @@ namespace Mandarin.ViewModels.MiniMandarin
         /// <summary>
         /// Initializes a new instance of the <see cref="MiniMandarinPageViewModel"/> class.
         /// </summary>
-        /// <param name="markdownPipeline">The markdown pipeline.</param>
-        public MiniMandarinPageViewModel(MarkdownPipeline markdownPipeline)
+        /// <param name="pageContentModel">The public website content model.</param>
+        public MiniMandarinPageViewModel(PageContentModel pageContentModel)
         {
-            var textContent = MiniMandarinPageViewModel.CreateTextContent();
-            this.TextContent = new MarkupString(Markdown.ToHtml(textContent, markdownPipeline));
-            this.BannerImageViewModel = new MandarinImageViewModel("/static/images/the-mini-mandarin/TheMiniMandarin-Banner.jpg", "The Mini Mandarin - Bearcarons");
-            this.MacaronImageViewModels = new List<IMandarinImageViewModel>
-            {
-                new MandarinImageViewModel("/static/images/the-mini-mandarin/TheMiniMandarin-Macaron-Chocolate.jpg", "The Mini Mandarin - Chocolate Macaron"),
-                new MandarinImageViewModel("/static/images/the-mini-mandarin/TheMiniMandarin-Macaron-Strawberry.jpg", "The Mini Mandarin - Strawberry Macaron"),
-                new MandarinImageViewModel("/static/images/the-mini-mandarin/TheMiniMandarin-Macaron-Matcha.jpg", "The Mini Mandarin - Matcha Macaron"),
-            }.AsReadOnly();
+            this.TextContent = pageContentModel.GetMarkupString("MiniMandarin", "MainText");
+            this.BannerImageViewModel = new MandarinImageViewModel(pageContentModel.Get<ImageUrlModel>("MiniMandarin", "BannerImage"));
+            this.MacaronImageViewModels = pageContentModel.GetAll<ImageUrlModel>("MiniMandarin", "MacaronImages")
+                                                          .Select(x => new MandarinImageViewModel(x))
+                                                          .ToList()
+                                                          .AsReadOnly();
         }
 
         /// <inheritdoc/>
@@ -33,17 +30,5 @@ namespace Mandarin.ViewModels.MiniMandarin
 
         /// <inheritdoc/>
         public IReadOnlyList<IMandarinImageViewModel> MacaronImageViewModels { get; }
-
-        private static string CreateTextContent()
-        {
-            return @"The Mini Mandarin is the younger sister of The Little Mandarin and is run by the baker of yummy
-treats in the family. Come drop by for handmade and hand finished ‘bearcarons’ – bear shaped macarons! Choose from
-three delightful flavours, chocolate, strawberry and matcha! Other special flavours pop up all year round and JUMBO
-sized macarons too, so come back to see what's new! The Mini Mandarin also offers a range of sweets and chocolates 
-suitable for vegetarians and vegans - there's a treat for everyone!
-
-You can follow The Mini Mandarin on Instagram [@theminimandarin_e17](https://instagram.com/theminimandarin_e17) to make
-sure you don't miss out!";
-        }
     }
 }
--- tests/Mandarin.ViewModels.Tests/MiniMandarin/MiniMandarinPageViewModelTests.cs
+++ tests/Mandarin.ViewModels.Tests/MiniMandarin/MiniMandarinPageViewModelTests.cs
@@ -1,7 +1,7 @@
-using System.Threading.Tasks;
-using AngleSharp;
-using AngleSharp.Dom;
+using System.Collections.Generic;
+using Bashi.Tests.Framework.Data;
 using Mandarin.ViewModels.MiniMandarin;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
 namespace Mandarin.ViewModels.Tests.MiniMandarin
@@ -10,27 +10,58 @@ namespace Mandarin.ViewModels.Tests.MiniMandarin
     public class MiniMandarinPageViewModelTests
     {
         [Test]
-        public async Task TextContent_ShouldBeHtmlContent()
+        public void TextContent_ShouldBeHtmlContent()
         {
-            var subject = new MiniMandarinPageViewModel(null);
-            var document = await BrowsingContext.New().OpenAsync(req => req.Content(subject.TextContent.Value));
-            var link = document.Body.QuerySelector("a");
-            Assert.That(link.Attributes["href"].Value, Is.EqualTo("https://instagram.com/theminimandarin_e17"));
+            var data = new
+            {
+                MiniMandarin = new
+                {
+                    MainText = "This *is* markdown!.",
+                    BannerImage = TestData.Create<ImageUrlModel>(),
+                    MacaronImages = TestData.Create<List<ImageUrlModel>>(),
+                },
+            };
+
+            var pageContentModel = new PageContentModel(null, JToken.FromObject(data));
+            var subject = new MiniMandarinPageViewModel(pageContentModel);
+            Assert.That(subject.TextContent.Value, Is.EqualTo("<p>This <em>is</em> markdown!.</p>\n"));
         }
 
         [Test]
         public void BannerImageViewModel_ShouldPointToBanner()
         {
-            var subject = new MiniMandarinPageViewModel(null);
-            Assert.That(subject.BannerImageViewModel.SourceUrl.ToString(), Contains.Substring("Banner"));
-            Assert.That(subject.BannerImageViewModel.Description, Contains.Substring("Bearcarons"));
+            var data = new
+            {
+                MiniMandarin = new
+                {
+                    MainText = TestData.NextString(),
+                    BannerImage = TestData.Create<ImageUrlModel>(),
+                    MacaronImages = TestData.Create<List<ImageUrlModel>>(),
+                },
+            };
+
+            var pageContentModel = new PageContentModel(null, JToken.FromObject(data));
+            var subject = new MiniMandarinPageViewModel(pageContentModel);
+            Assert.That(subject.BannerImageViewModel.SourceUrl.ToString(), Is.EqualTo(data.MiniMandarin.BannerImage.Url));
+            Assert.That(subject.BannerImageViewModel.Description, Is.EqualTo(data.MiniMandarin.BannerImage.Description));
         }
 
         [Test]
-        public void MacaronImageViewModels_ShouldHave3Images()
+        public void MacaronImageViewModels_ShouldHaveMatchingNumberOfImages()
         {
-            var subject = new MiniMandarinPageViewModel(null);
-            Assert.That(subject.MacaronImageViewModels, Has.Count.EqualTo(3));
+            var data = new
+            {
+                MiniMandarin = new
+                {
+                    MainText = TestData.NextString(),
+                    BannerImage = TestData.Create<ImageUrlModel>(),
+                    MacaronImages = TestData.Create<List<ImageUrlModel>>(),
+                },
+            };
+
+            var pageContentModel = new PageContentModel(null, JToken.FromObject(data));
+            var subject = new MiniMandarinPageViewModel(pageContentModel);
+            Assert.That(subject.MacaronImageViewModels, Has.Count.EqualTo(data.MiniMandarin.MacaronImages.Count));
         }
     }
 }
