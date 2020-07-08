a402f57ae10b4d44c17e9f0dc9b6d223ab83ee12
KK578 <KevinKwan95@googlemail.com>
2020-07-08 12:02:03 +0100

    BASHI-69: Implement cache inspection/cleanup page  - Fix all caches not properly expiring automatically.  - Set new default cache time to 1 hour. | Add Cache Expiry Page

---
+++ src/Mandarin.Services/Decorators/CachingDecoratorBase.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using LazyCache;
+using Mandarin.Services.Square;
+using Microsoft.Extensions.Logging;
+
+namespace Mandarin.Services.Decorators
+{
+    /// <summary>
+    /// Represents common functionality shared by all caching decorators.
+    /// </summary>
+    internal abstract class CachingDecoratorBase
+    {
+        private readonly IAppCache appCache;
+        private readonly ILogger<CachingDecoratorBase> logger;
+        private readonly SemaphoreSlim semaphore;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CachingDecoratorBase"/> class.
+        /// </summary>
+        /// <param name="appCache">The application caching service.</param>
+        /// <param name="logger">The application logger.</param>
+        protected CachingDecoratorBase(IAppCache appCache, ILogger<CachingDecoratorBase> logger)
+        {
+            this.appCache = appCache;
+            this.logger = logger;
+            this.semaphore = new SemaphoreSlim(1);
+        }
+
+        /// <summary>
+        /// Gets the existing values from the application cache, or creates it fresh from the provided factory method.
+        /// </summary>
+        /// <typeparam name="T">The type of the values to be cached.</typeparam>
+        /// <param name="key">The key to identify the value in the cache.</param>
+        /// <param name="addItemFactory">The factory method to create a new instance.</param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        protected Task<IReadOnlyList<T>> GetOrAddAsync<T>(string key, Func<Task<IEnumerable<T>>> addItemFactory)
+        {
+            return this.appCache.GetOrAddAsync(key, AddItemFactory, DateTimeOffset.Now.AddHours(1));
+
+            async Task<IReadOnlyList<T>> AddItemFactory()
+            {
+                try
+                {
+                    await this.semaphore.WaitAsync();
+                    var result = (await addItemFactory()).NullToEmpty().ToList().AsReadOnly();
+                    this.logger.LogInformation("Adding Cache Entry '{Key}' with {Count} Items", key, result.Count);
+
+                    var existingKeys = await this.appCache.GetAsync<List<string>>("Cache.Keys");
+                    var keys = existingKeys.NullToEmpty().Append(key).Distinct().ToList();
+                    this.appCache.Add("Cache.Keys", keys, DateTimeOffset.MaxValue);
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    this.logger.LogError(ex, "Adding Cache Entry '{Key}' failed.", key);
+                    return Enumerable.Empty<T>().NullToEmpty().ToList().AsReadOnly();
+                }
+                finally
+                {
+                    this.semaphore.Release();
+                }
+            }
+        }
+    }
+}
--- src/Mandarin.Services/Decorators/CachingInventoryServiceDecorator.cs
+++ src/Mandarin.Services/Decorators/CachingInventoryServiceDecorator.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Threading.Tasks;
 using System.Runtime.CompilerServices;
@@ -8,27 +7,27 @@ using System.Threading.Tasks;
 using LazyCache;
 using Mandarin.Models.Commissions;
 using Mandarin.Models.Inventory;
-using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 
 namespace Mandarin.Services.Decorators
 {
     /// <summary>
     /// Decorated implementation of <see cref="IInventoryService"/> that caches results to speed up subsequent requests.
     /// </summary>
-    internal sealed class CachingInventoryServiceDecorator : IQueryableInventoryService
+    internal sealed class CachingInventoryServiceDecorator : CachingDecoratorBase, IQueryableInventoryService
     {
         private readonly IInventoryService inventoryService;
-        private readonly IAppCache appCache;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CachingInventoryServiceDecorator"/> class.
         /// </summary>
         /// <param name="inventoryService">The inventory service to be decorated.</param>
         /// <param name="appCache">The application memory cache.</param>
-        public CachingInventoryServiceDecorator(IInventoryService inventoryService, IAppCache appCache)
+        /// <param name="logger">The application logger.</param>
+        public CachingInventoryServiceDecorator(IInventoryService inventoryService, IAppCache appCache, ILogger<CachingInventoryServiceDecorator> logger)
+            : base(appCache, logger)
         {
             this.inventoryService = inventoryService;
-            this.appCache = appCache;
         }
 
         /// <inheritdoc/>
@@ -55,23 +54,9 @@ namespace Mandarin.Services.Decorators
         /// <inheritdoc/>
         public IObservable<FixedCommissionAmount> GetFixedCommissionAmounts()
         {
-            return Observable.FromAsync(() => this.appCache.GetOrAddAsync(this.CreateCacheKey(), CreateEntry))
-                             .SelectMany(x => x);
-
-            async Task<IReadOnlyList<FixedCommissionAmount>> CreateEntry(ICacheEntry e)
-            {
-                try
-                {
-                    e.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
-                    var result = await this.inventoryService.GetFixedCommissionAmounts().ToList().ToTask();
-                    return result.ToList().AsReadOnly();
-                }
-                catch (Exception)
-                {
-                    e.AbsoluteExpiration = DateTimeOffset.MinValue;
-                    return new List<FixedCommissionAmount>().AsReadOnly();
-                }
-            }
+            return this.GetOrAddAsync(this.CreateCacheKey(), () => this.inventoryService.GetFixedCommissionAmounts().ToList().ToTask<IEnumerable<FixedCommissionAmount>>())
+                       .ToObservable()
+                       .SelectMany(x => x);
         }
 
         /// <inheritdoc/>
@@ -83,23 +68,9 @@ namespace Mandarin.Services.Decorators
         /// <inheritdoc/>
         public IObservable<Product> GetInventory()
         {
-            return Observable.FromAsync(() => this.appCache.GetOrAddAsync(this.CreateCacheKey(), CreateEntry))
-                             .SelectMany(x => x);
-
-            async Task<IReadOnlyList<Product>> CreateEntry(ICacheEntry e)
-            {
-                try
-                {
-                    e.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
-                    var result = await this.inventoryService.GetInventory().ToList().ToTask();
-                    return result.ToList().AsReadOnly();
-                }
-                catch (Exception)
-                {
-                    e.AbsoluteExpiration = DateTimeOffset.MinValue;
-                    return new List<Product>().AsReadOnly();
-                }
-            }
+            return this.GetOrAddAsync(this.CreateCacheKey(), () => this.inventoryService.GetInventory().ToList().ToTask<IEnumerable<Product>>())
+                       .ToObservable()
+                       .SelectMany(x => x);
         }
 
         /// <inheritdoc/>
