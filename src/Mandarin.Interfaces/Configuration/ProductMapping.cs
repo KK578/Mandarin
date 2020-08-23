a8822d5ae36b2852832f80d059b75d49706fccbc
Kevin Kwan <KevinKwan95@googlemail.com>
2020-08-23 20:53:06 +0100

    BASHI-85: Add Product Mappings for Commission to Wholesale Transfer. (#215)

---
+++ src/Mandarin.Interfaces/Configuration/ProductMapping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mandarin.Configuration
+{
+    /// <summary>
+    /// Contains the set of mappings to be applied to transactions that occur after a specific date.
+    /// </summary>
+    public class ProductMapping
+    {
+        /// <summary>
+        /// Gets or sets the date from which these mappings should be applied.
+        /// </summary>
+        public DateTime TransactionsAfterDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the map of transaction product code to their corrected product codes.
+        /// </summary>
+        public Dictionary<string, string> Mappings { get; set; }
+    }
+}
