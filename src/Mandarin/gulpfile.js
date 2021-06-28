09be6eb5a35ad1a83791a42d12008ce189a5d4ad
KK578 <KevinKwan95@googlemail.com>
2021-06-29 00:04:10 +0100

    BASHI-119: Remove all references of tailwindcss | Fix Difficult to Read Text when Selecting a Row in a Grid

--- src/Mandarin/gulpfile.js
+++ src/Mandarin/gulpfile.js
@@ -69,32 +69,6 @@ async function buildInformation() {
     fs.writeFileSync("BuildInformation.json", JSON.stringify(info));
 }
 
-function css() {
-    const debug = require("gulp-debug");
-    const postcss = require("gulp-postcss");
-    const cleanCss = require("gulp-clean-css");
-    const sourceMaps = require("gulp-sourcemaps");
-    const rename = require("gulp-rename");
-
-    return gulp.src("./Styles/*.pcss")
-        .pipe(debug({
-            title: "src:"
-        }))
-        .pipe(postcss([
-            require("precss"),
-            require("tailwindcss"),
-            require("autoprefixer")
-        ]))
-        .pipe(rename({extname: ".css"}))
-        .pipe(sourceMaps.init())
-        .pipe(cleanCss())
-        .pipe(sourceMaps.write("./"))
-        .pipe(gulp.dest("./wwwroot/css/"))
-        .pipe(debug({
-            title: "output:"
-        }))
-}
-
 gulp.task(buildInformation);
 gulp.task(css);
 gulp.task("default", css);
