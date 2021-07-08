778e56bcd2ae4b0d5f5f771d00d9e4d86b9203d8
KK578 <KevinKwan95@googlemail.com>
2021-07-08 22:48:51 +0100

    BASHI-125: Fix CS1591 errors | Refactor Artist pages to use MVVM

--- src/Mandarin.Client/Components/Inputs/MandarinIcon.cs
+++ src/Mandarin.Client/Components/Inputs/MandarinIcon.cs
@@ -1,10 +1,13 @@
-﻿using IconStyle = Blazorise.IconStyle;
+﻿using Blazorise;
 
 namespace Mandarin.Client.Components.Inputs
 {
     /// <summary>
     /// Represents a wrapper for a FontAwesome Icon.
     /// </summary>
+    /// <param name="IconClass">The CSS class family that the FontAwesome Icon belongs to.</param>
+    /// <param name="IconName">The CSS class that identifies the FontAwesome Icon.</param>
+    /// <param name="IconStyle">The Icon style weight to be set.</param>
     public sealed record MandarinIcon(string IconClass, string IconName, IconStyle IconStyle)
     {
         /// <summary>
