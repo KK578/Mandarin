12430745805dec54149ef8239ae2953f105a6595
Kevin Kwan <KevinKwan95@googlemail.com>
2021-07-18 21:13:21 +0100

    BASHI-127: Migrate all validation to FluentValidation. (#500)

--- src/Mandarin.Interfaces/Commissions/Commission.cs
+++ src/Mandarin.Interfaces/Commissions/Commission.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 using Mandarin.Stockists;
 
 namespace Mandarin.Commissions
@@ -12,31 +11,26 @@ namespace Mandarin.Commissions
         /// <summary>
         /// Gets the commission's Database ID.
         /// </summary>
-        [Key]
         public CommissionId CommissionId { get; init; }
 
         /// <summary>
         /// Gets the related stockist ID related to this commission.
         /// </summary>
-        [Required]
         public StockistId StockistId { get; init; }
 
         /// <summary>
         /// Gets the start date for this commission.
         /// </summary>
-        [Required]
         public DateTime StartDate { get; init; }
 
         /// <summary>
         /// Gets the end date for this commission.
         /// </summary>
-        [Required]
         public DateTime EndDate { get; init; }
 
         /// <summary>
         /// Gets the agreed commission rate group for this commission.
         /// </summary>
-        [Required]
         public int Rate { get; init; }
 
         /// <summary>
--- src/Mandarin.Interfaces/Stockists/StockistDetail.cs
+++ src/Mandarin.Interfaces/Stockists/StockistDetail.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Mandarin.Stockists
+﻿namespace Mandarin.Stockists
 {
     /// <summary>
     /// Represents a stockist's personal information.
@@ -10,64 +8,51 @@ namespace Mandarin.Stockists
         /// <summary>
         /// Gets the stockist's Database ID.
         /// </summary>
-        [Key]
         public StockistId StockistId { get; init; }
 
         /// <summary>
         /// Gets the Stockist's first name.
         /// </summary>
-        [StringLength(100)]
         public string FirstName { get; init; }
 
         /// <summary>
         /// Gets the Stockist's last name.
         /// </summary>
-        [StringLength(100)]
         public string LastName { get; init; }
 
         /// <summary>
         /// Gets the Stockist's artist/display name.
         /// </summary>
-        [Required]
-        [StringLength(250)]
         public string DisplayName { get; init; }
 
         /// <summary>
         /// Gets the stockist's twitter handle.
         /// </summary>
-        [MaxLength(30)]
         public string TwitterHandle { get; init; }
 
         /// <summary>
         /// Gets the stockist's instagram handle.
         /// </summary>
-        [MaxLength(30)]
         public string InstagramHandle { get; init; }
 
         /// <summary>
         /// Gets the stockist's facebook handle.
         /// </summary>
-        [MaxLength(30)]
         public string FacebookHandle { get; init; }
 
         /// <summary>
         /// Gets the stockist's personal website url.
         /// </summary>
-        [Url]
-        [MaxLength(150)]
         public string WebsiteUrl { get; init; }
 
         /// <summary>
         /// Gets the stockist's tumblr handle.
         /// </summary>
-        [MaxLength(30)]
         public string TumblrHandle { get; init; }
 
         /// <summary>
         /// Gets the stockist's email address.
         /// </summary>
-        [EmailAddress]
-        [MaxLength(100)]
         public string EmailAddress { get; init; }
     }
 }
