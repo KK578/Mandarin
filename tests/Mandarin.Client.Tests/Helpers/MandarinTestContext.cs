19739c126557f8c9004dc3791774eac27b8d831b
KK578 <KevinKwan95@googlemail.com>
2021-08-19 23:01:04 +0100

    BASHI-139: Add full tests for Index page | Add bUnit tests for all Blazor pages

---
+++ tests/Mandarin.Client.Tests/Helpers/MandarinTestContext.cs
@@ -0,0 +1,15 @@
+﻿using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Bunit;
+
+namespace Mandarin.Client.Tests.Helpers
+{
+    public abstract class MandarinTestContext : TestContext
+    {
+        protected MandarinTestContext()
+        {
+            this.Services.AddBlazorise().AddBootstrapProviders().AddFontAwesomeIcons();
+        }
+    }
+}
--- tests/Mandarin.Client.Tests/Pages/IndexTests.cs
+++ tests/Mandarin.Client.Tests/Pages/IndexTests.cs
@@ -1,8 +1,8 @@
-﻿using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
+﻿using System;
+using Blazorise;
 using Bunit;
 using FluentAssertions;
+using Mandarin.Client.Tests.Helpers;
 using Mandarin.Client.ViewModels.Index;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
@@ -11,19 +11,44 @@ using Index = Mandarin.Client.Pages.Index;
 
 namespace Mandarin.Client.Tests.Pages
 {
-    public class IndexTests : TestContext
+    public sealed class IndexTests : MandarinTestContext
     {
+        private readonly Mock<IIndexViewModel> indexViewModel = new();
+
+        public IndexTests()
+        {
+            this.Services.AddSingleton(this.indexViewModel.Object);
+        }
+
+        private IRenderedComponent<Index> Subject => this.RenderComponent<Index>();
+
         [Fact]
         public void ShouldContainTheLogo()
         {
-            var viewModel = new Mock<IIndexViewModel>();
-            this.Services.AddSingleton(viewModel.Object);
-            this.Services.AddBlazorise().AddBootstrapProviders().AddFontAwesomeIcons();
-
-            var cut = this.RenderComponent<Index>();
-
-            var image = cut.FindComponent<FigureImage>();
+            var image = this.Subject.FindComponent<FigureImage>();
             image.Instance.Source.Should().Be("/static/images/logo.png");
+        }
+
+        [Fact]
+        public void ShouldContainTheCompanyName()
+        {
+            var title = this.Subject.FindComponent<JumbotronTitle>();
+            title.Markup.Should().Contain("The Little Mandarin");
+        }
+
+        [Fact]
+        public void ShouldDisplayTheApplicationVersion()
+        {
+            this.indexViewModel.Setup(x => x.Version).Returns(new Version(1, 2, 3, 4));
+            this.Subject.Markup.Should().Contain("version 1.2.3.4");
+        }
+
+        [Fact]
+        public void ShouldContainTheIconToDisplay()
+        {
+            this.indexViewModel.Setup(x => x.Icon).Returns(IconName.Brush);
+            var icon = this.Subject.FindComponent<Icon>();
+            icon.Instance.Name.Should().Be(IconName.Brush);
         }
     }
 }
