45931c3b4166baf5c41081b1c184598f0e7e1f53
KK578 <KevinKwan95@googlemail.com>
2021-08-19 22:48:04 +0100

    BASHI-139: Add rudimentary basic test with the index page | Add bUnit tests for all Blazor pages

---
+++ tests/Mandarin.Client.Tests/Pages/IndexTests.cs
@@ -0,0 +1,29 @@
+﻿using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Bunit;
+using FluentAssertions;
+using Mandarin.Client.ViewModels.Index;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+using Index = Mandarin.Client.Pages.Index;
+
+namespace Mandarin.Client.Tests.Pages
+{
+    public class IndexTests : TestContext
+    {
+        [Fact]
+        public void ShouldContainTheLogo()
+        {
+            var viewModel = new Mock<IIndexViewModel>();
+            this.Services.AddSingleton(viewModel.Object);
+            this.Services.AddBlazorise().AddBootstrapProviders().AddFontAwesomeIcons();
+
+            var cut = this.RenderComponent<Index>();
+
+            var image = cut.FindComponent<FigureImage>();
+            image.Instance.Source.Should().Be("/static/images/logo.png");
+        }
+    }
+}
