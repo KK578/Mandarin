e82af8849bb15a90c621a763c3d3024008ada9a1
KK578 <KevinKwan95@googlemail.com>
2021-08-22 11:34:54 +0100

    BASHI-136: Add Autofac for Mandarin.Interfaces | Migrate container to Autofac

---
+++ src/Mandarin.Interfaces/MandarinInterfacesModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using AutoMapper;
+using Mandarin.Converters;
+
+namespace Mandarin
+{
+    /// <summary>
+    /// Represents an Autofac <see cref="Module"/> which registers all Mandarin.Interfaces services.
+    /// </summary>
+    public sealed class MandarinInterfacesModule : Module
+    {
+        /// <inheritdoc />
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterType<MandarinTinyTypeMapperProfile>().As<Profile>();
+        }
+    }
+}
