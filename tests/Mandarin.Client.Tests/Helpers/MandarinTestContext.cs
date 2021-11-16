0d9d74d07d5781b82de5c6315e4efd9f21060e3f
Kevin Kwan <KevinKwan95@googlemail.com>
2021-11-16 21:57:51 +0000

    BASHI-139: Add bUnit tests (#522)

---
+++ tests/Mandarin.Client.Tests/Helpers/MandarinTestContext.cs
@@ -0,0 +1,34 @@
+﻿using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using ReactiveUI;
+
+namespace Mandarin.Client.Tests.Helpers
+{
+    public abstract class MandarinTestContext<TView, TViewModel> : TestContext
+        where TView : IComponent
+        where TViewModel : class, IReactiveObject
+    {
+        protected MandarinTestContext()
+        {
+            this.Services.AddSingleton(this.ViewModel.Object);
+            this.SetupBlazorise();
+        }
+
+        protected Mock<TViewModel> ViewModel { get; } = new();
+
+        protected IRenderedComponent<TView> Subject => this.RenderComponent<TView>();
+
+        private void SetupBlazorise()
+        {
+            this.Services.AddBlazorise().AddBootstrapProviders().AddFontAwesomeIcons();
+
+            this.JSInterop.Setup<bool>("blazorise.button.initialize", _ => true);
+            this.JSInterop.Setup<bool>("blazorise.textEdit.initialize", _ => true);
+        }
+    }
+}
---
+++ tests/Mandarin.Client.Tests/Pages/IndexTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Blazorise;
+using Bunit;
+using FluentAssertions;
+using Mandarin.Client.Tests.Helpers;
+using Mandarin.Client.ViewModels.Index;
+using Xunit;
+using Index = Mandarin.Client.Pages.Index;
+
+namespace Mandarin.Client.Tests.Pages
+{
+    public sealed class IndexTests : MandarinTestContext<Index, IIndexViewModel>
+    {
+        [Fact]
+        public void ShouldContainTheLogo()
+        {
+            var image = this.Subject.FindComponent<FigureImage>();
+            image.Instance.Source.Should().Be("/static/images/logo.png");
+        }
+
+        [Fact]
+        public void ShouldContainTheCompanyName()
+        {
+            var title = this.Subject.FindComponent<JumbotronTitle>();
+            title.Markup.Should().Contain("The Little Mandarin");
+        }
+
+        [Fact]
+        public void ShouldDisplayTheApplicationVersion()
+        {
+            this.ViewModel.Setup(x => x.Version).Returns(new Version(1, 2, 3, 4));
+            this.Subject.Markup.Should().Contain("version 1.2.3.4");
+        }
+
+        [Fact]
+        public void ShouldContainTheIconToDisplay()
+        {
+            this.ViewModel.Setup(x => x.Icon).Returns(IconName.Brush);
+            var icon = this.Subject.FindComponent<Icon>();
+            icon.Instance.Name.Should().Be(IconName.Brush);
+        }
+    }
+}
