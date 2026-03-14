c3eadf36d2ace0b58bebaeeab2924e106e57ca34
Kevin Kwan <KevinKwan95@googlemail.com>
2026-03-14 22:19:40 +0000

    Rename public-app to mandarin-admin

--- mandarin-admin/src/app/layout.tsx
+++ mandarin-admin/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { NavBar, UserProfile } from "./components/NavBar";
+import { NavBar } from "./components/NavBar";
 import "./globals.css";
 
 const geistSans = Geist({
