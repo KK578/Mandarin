973d8dfa0862423ac87fb76b495a5291d7088aac
Kevin Kwan <KevinKwan95@googlemail.com>
2026-03-16 22:18:46 +0000

    Update the front page branding (#797)

--- pomelo/src/app/page.tsx
+++ pomelo/src/app/page.tsx
@@ -1,6 +1,29 @@
 import { auth0 } from "@/lib/auth0";
 import { AuthGate } from "./components/AuthGate";
 
+const Grid: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
+      {children}
+    </div>
+  );
+};
+
+const LinkCard: React.FC<
+  React.PropsWithChildren<{ href: string; title: string }>
+> = ({ children, href, title }) => {
+  return (
+    <a href={href}>
+      <div className="card bg-base-300">
+        <div className="card-body">
+          <h2 className="card-title text-xl">{title}</h2>
+          {children}
+        </div>
+      </div>
+    </a>
+  );
+};
+
 export default async function Home() {
   const session = await auth0.getSession();
 
@@ -9,16 +32,31 @@ export default async function Home() {
   }
 
   return (
-    <div className="hero h-full bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Welcome to the new admin app for The Little Mandarin. This is
-            currently a placeholder.
-          </p>
+    <section className="h-full w-full p-4">
+      <Grid>
+        <div className="card bg-base-300 col-span-full">
+          <div className="card-body">
+            <h2 className="card-title text-xl">
+              Welcome to the Admin Dashboard.
+            </h2>
+            <p>Hopefully all of this will start working soon...</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Primary</button>
+              <button className="btn btn-secondary">Secondary</button>
+              <button className="btn btn-accent">Accent</button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+        <LinkCard href="/artists" title="Artists">
+          <p>Add or manage artist details and commission rates.</p>
+        </LinkCard>
+        <LinkCard href="/inventory" title="Inventory">
+          <p>Manage the product inventory and frame prices.</p>
+        </LinkCard>
+        <LinkCard href="/record-of-sales" title="Record of Sales">
+          <p>View and Records of Sales and send emails.</p>
+        </LinkCard>
+      </Grid>
+    </section>
   );
 }
